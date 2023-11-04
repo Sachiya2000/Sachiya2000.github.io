8684fdc39a490d459d3990ff6d44d4effe17b6ad
Sachitha Madhuranga <118953518+Sachiya2000@users.noreply.github.com>
2023-11-04 13:02:11 +0530

    Updates

---
+++ precache-manifest.0ad91f9d9b046d9fb00ce7d2830ebd82.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1aa98850840951d5296c7fd6627232",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6fbf9cb5902dd4ad91f",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "2e46ef51ffa20fc21eff",
+    "url": "/static/css/main.7d96fc6e.chunk.css"
+  },
+  {
+    "revision": "e6fbf9cb5902dd4ad91f",
+    "url": "/static/js/2.728f283f.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/2.728f283f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e46ef51ffa20fc21eff",
+    "url": "/static/js/main.fc771357.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.fc771357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f750a439bab9387a9253",
+    "url": "/static/js/runtime-main.3174f3e9.js"
+  },
+  {
+    "revision": "d584f24224d90671bed4403c31fdcf96",
+    "url": "/static/media/Sachiya.d584f242.jpg"
+  },
+  {
+    "revision": "b931dd24f43764b277fa1cd4d29c241d",
+    "url": "/static/media/img2.b931dd24.jpg"
+  },
+  {
+    "revision": "b931dd24f43764b277fa1cd4d29c241d",
+    "url": "/static/media/imgbackground.b931dd24.jpg"
+  }
+]);
